6afdc0808616c531426d95dcfc8b304e1f2bbc2f
namcneill <nathan.mcneill1@gmail.com>
2021-12-19 12:12:02 -0600

    added data normalization, plotting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from pprintpp import pprint
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def ticker_to_path(data_path, ticker):
@@ -13,13 +15,34 @@ def get_data(data_path, tickers, date_range):
     for ticker in tickers:
         ticker_path = ticker_to_path(data_path, ticker)
 
-        df_temp = pd.read_csv(ticker_path, index_col='Date', parse_dates=True, usecols=['Date', 'Adj Close'], na_values=['nan'])
+        df_temp = pd.read_csv(ticker_path,
+                              index_col='Date',
+                              parse_dates=True,
+                              usecols=['Date', 'Adj Close'],
+                              na_values=['nan'])
         df_temp = df_temp.rename(columns={'Adj Close': ticker})
         df_final = df_final.join(df_temp)
         if ticker == 'SPY':
             df_final = df_final.dropna(subset=['SPY'])
 
     return df_final
+
+
+def normalize_data(data):
+    # index = date, columns = ticker
+    return data.iloc[1:] / data.iloc[0]
+
+
+def plot_data(data, x_label, y_label, title):
+    data.plot()
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.legend(loc='best')
+    plt.show()
+
+
+def moving_avg(period):
 
 
 def main():
@@ -29,7 +52,10 @@ def main():
     end_date = '2021-12-12'
     date_range = pd.date_range(start_date, end_date)
     df_data = get_data(data_dir, tickers, date_range)
-    pprint(df_data)
+    norm_data = normalize_data(df_data)
+    pprint(norm_data)
+    plot_data(norm_data, x_label='Time', y_label='Normalized Gains', title='Normalized TechStock Growth')
+    plot_data(df_data, x_label='Time', y_label='Gains', title='TechStock Growth')
 
 
 if __name__ == '__main__':
